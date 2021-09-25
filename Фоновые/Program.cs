1d95db53c5dd610817f6510a0c85108615a16712
K0pyt0 <ivan@alick.ru>
2021-09-25 20:16:54 +0300

    Initial commit

---
+++ Фоновые/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+//using Math;
+
+namespace Практика2
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            int c = int.Parse(Console.ReadLine());
+
+
+            Console.WriteLine($"{((a - b) / (double)(c) + Math.Sqrt(c + a / Math.Sin(c / 4.0) - 0.2)):F2}");
+            
+        }
+    }
+}
+//6 вариант
---
+++ Фоновые2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Фоновые2
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
