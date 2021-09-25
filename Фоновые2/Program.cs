daf1379ad37cf51ce519b1a9e8f39b49de2cdd9c
K0pyt0 <ivan@alick.ru>
2021-09-25 23:09:23 +0300

    Second
    Second task done

--- Фоновые2/Program.cs
+++ Фоновые2/Program.cs
@@ -6,7 +6,24 @@ namespace Фоновые2
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            double x = double.Parse(Console.ReadLine());
+            double y = double.Parse(Console.ReadLine());
+            double r1 = 1;
+            double r2 = 2;
+            double ans;
+
+            if (y > 0)
+            {
+                if (Math.Pow(x, 2) + Math.Pow(x, 2) >= Math.Pow(r1, 2))
+                {
+                    if (Math.Pow(x, 2) + Math.Pow(x, 2) <= Math.Pow(r2, 2)) ans = 1 / (5 * x + y);
+                    else ans = Math.Abs(1 - x * y);
+                }
+                else ans = Math.Abs(1 - x * y);
+            }
+            else ans = Math.Abs(1 - x * y);
+
+            Console.WriteLine(ans);
         }
     }
 }
