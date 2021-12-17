9aa2754a868335370be6af6cd9bbd89790883c78
K0pyt0 <ivan@alick.ru>
2021-12-17 15:07:19 +0300

    Task 31 addition solved, 32 solving beginned

---
+++ Фоновая31/Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+
+namespace Фоновая31
+{
+    class Program
+    {
+        static string Reverse(string num)
+        {
+            string tmp = "";
+            for (int i = num.Length - 1; i >= 0; i--)
+            {
+                tmp += num[i];
+            }
+            return tmp;
+        }
+
+        static int ToNormType(char el)
+        {
+            return int.Parse(Convert.ToString(el));
+        }
+
+        static void ToOneLength(ref string num1, ref string num2)
+        {
+            num1 = Reverse(num1);
+            num2 = Reverse(num2);
+            while (num1.Length != num2.Length)
+            {
+                if (num1.Length > num2.Length) num2 += " ";
+                else num1 += " ";
+            }
+        }
+
+        static string Sum(string num1, string num2)
+        {
+            string ans = "";
+            int leave = 0;
+            for (int i = 0; i < num1.Length; i++)
+            {
+                int plus = ToNormType(num1[i]) + ToNormType(num2[i]) + leave;
+                int add = plus % 10;
+                ans += Convert.ToString(add);
+                leave = plus / 10;
+            }
+            if (leave == 1)
+            {
+                ans += "1";
+            }
+            return Reverse(ans);
+        }
+
+        static string Decr(string num1, string num2)
+        {
+            string ans = "";
+            int take = 0;
+            for (int i = 0; i < num1.Length; i++)
+            {
+                int end = ToNormType(num1[i]) - ToNormType(num2[i]) - take;
+                int add = 0;
+                if (end >= 0)
+                {
+                    take = 0;
+                    add = end;
+                }
+                else
+                {
+                    take = 1;
+                    add = (end + 10) % 10;
+                }
+                ans += Convert.ToString(add);
+
+            }
+            return Reverse(ans);
+        }
+
+        static void Main(string[] args)
+        {
+            string num1 = Console.ReadLine();
+            string num2 = Console.ReadLine();
+            string oper = Console.ReadLine();
+            ToOneLength(ref num1, ref num2);
+
+            if (oper == "+") Console.WriteLine(Sum(num1, num2));
+            if (oper == "-") Console.WriteLine(Decr(num1, num2));
+            Console.ReadKey();
+        }
+
+    }
+}
---
+++ Фоновая32/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+
+namespace Фоновая32
+{
+    class MainClass
+    {
+        static int[] InputArr(int N)
+        {
+            Console.Write("Введите длину массива");
+            //int N = int.Parse(Console.ReadLine());
+            int[] arr = new int[N];
+            for (int i = 0; i < N; i++)
+            {
+                arr[i] = int.Parse(Console.ReadLine());
+            }
+            return arr;
+        }
+
+        static void PrintArr(int[] arr)
+        {
+            foreach (var i in arr)
+            {
+                Console.Write($"{i} ");
+            }
+        }
+
+        static void SumBtwMinMax()
+        {
+            Console.Write("Введите длину массива");
+            int N = int.Parse(Console.ReadLine());
+            int[] arr = InputArr(N);
+
+
+            int sum = 0;
+            Console.WriteLine(sum);
+        }
+
+        static int[] Move()
+        {
+            Console.Write("Введите длину массива");
+            int N = int.Parse(Console.ReadLine());
+            Console.WriteLine("Введите сдвиг");
+            int k = int.Parse(Console.ReadLine());
+            int[] arr = InputArr(N + k);
+
+            int[] finArr = new int[arr.Length + k];
+            //for (int i = )
+            return finArr;
+        }
+
+        static void Common()
+        {
+            int[] arr1 = InputArr();
+            int[] arr2 = InputArr();
+            for(int i = 0; i < arr1.Length; i++)
+            {
+                bool isThereSame = false;
+                foreach (var j in arr2)
+                {
+                    if (arr1[i] == j)
+                    {
+                        isThereSame = true;
+                        break;
+                    }
+                }
+                if(isThereSame)
+                {
+                    Console.Write($"{arr1[i]} ");
+                }
+            }
+            Console.WriteLine();
+            Console.WriteLine();
+        }
+
+        public static void Main(string[] args)
+        {
+            SumBtwMinMax();
+        }
+    }
+}
